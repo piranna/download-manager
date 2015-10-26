7644716179adc9299fe1e2604fcadb1b93198b1e
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-10-26 18:56:12 +0100

    Support to automatically patch files

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-var exists = require('fs').exists
-var join   = require('path').join
+var fs   = require('fs')
+var join = require('path').join
 
 var async    = require('async')
 var Download = require('download')
+var got      = require('got')
 var progress = require('download-status')
+
+var applyPatches = require('diff').applyPatches
 
 
 function getName(item)
 {
   return item.name
-}
-
-function getAction(item)
-{
-  return item.action.bind(item)
 }
 
 function getNames(downloads)
@@ -28,9 +26,9 @@ function getNames(downloads)
   return result + last
 }
 
-function notUndefinedAction(item)
+function hasAction(item)
 {
-  return item.action !== undefined
+  return item.patch || item.action
 }
 
 
@@ -50,10 +48,53 @@ function manager(downloads, options, callback)
     this.get(item.url, join(deps, item.name))
   }
 
+  function getAction(item)
+  {
+    if(item.action)
+    {
+      var action = item.action.bind(item)
+      if(!item.patch) return action
+    }
+
+
+    var name = item.name
+
+    return function(callback)
+    {
+      got(item.patch, function(error, patch)
+      {
+        if(error) return callback(error)
+
+        applyPatches(patch,
+        {
+          loadFile: function(patch, callback)
+          {
+            fs.readFile(join(deps, name, patch.index), 'utf8', callback)
+          },
+
+          patched: function(patch, content)
+          {
+            if(content === false)
+              return console.error('Context sanity check failed:',patch)
+
+            fs.writeFile(join(deps, name, patch.index), content)
+          },
+
+          complete: function(error)
+          {
+            if(error) return callback(error)
+
+            if(action) action.call(item, callback)
+          }
+        })
+      })
+    }
+  }
+
 
   async.reject(downloads, function(item, callback)
   {
-    exists(join(deps, item.name), callback)
+    fs.exists(join(deps, item.name), callback)
   },
   function(downloads)
   {
@@ -72,7 +113,7 @@ function manager(downloads, options, callback)
 
       if(process.env.CI) console.log('Done')
 
-      async.series(downloads.filter(notUndefinedAction).map(getAction),callback)
+      async.series(downloads.filter(hasAction).map(getAction),callback)
     })
   })
 }
