04f678fe5800d2dbae28282d7aaf79f3ff5cc56b
Jesús Leganés Combarro <piranna@gmail.com>
2016-04-12 12:34:34 +0200

    backup

--- index.js
+++ index.js
@@ -1,18 +1,79 @@
 var fs   = require('fs')
 var join = require('path').join
 
-var async     = require('async')
-var checksum  = require('download-checksum')
-var Download  = require('download')
-var got       = require('got')
-var progress  = require('download-status')
-var stripDirs = require('strip-dirs')
-
-var applyPatches = require('diff').applyPatches
+var applyPatches  = require('diff').applyPatches
+var async         = require('async')
+var checksums     = require('download-checksum').checksums
+var extract       = require('tar-fs').extract
+var forceArray    = require('force-array');
+var got           = require('got')
+var gunzip        = require('gunzip-maybe')
+var Multiprogress = require('multi-progress')
+var rimraf        = require('rimraf')
+var stripDirs     = require('strip-dirs')
 
 
 const CI = process.env.CI
 
+
+function noop(){}
+
+
+function getAction(item, deps)
+{
+  // Item has an action
+  var action = item.action
+  if(action) action = action.bind(item)
+
+  // No patch, return plain action (if any)
+  var patch = item.patch
+  if(!patch) return action
+
+  // Download and apply patch before exec action (if any)
+  var name  = item .name
+  var path  = patch.path  || item.path  || ''
+  var strip = patch.strip || item.strip || 0
+  var url   = patch.url   || patch
+
+  function loadFile(patch, callback)
+  {
+    var filename = stripDirs(patch.oldFileName, strip)
+    fs.readFile(join(deps, name, path, filename), 'utf8', callback)
+  }
+
+  function patched(patch, content)
+  {
+    if(content === false)
+      return console.error('Context sanity check failed:',patch)
+
+    var filename = stripDirs(patch.newFileName, strip)
+    fs.writeFile(join(deps, name, path, filename), content)
+  }
+
+  return function(callback)
+  {
+    function complete(error)
+    {
+      if(error) return callback(error)
+
+      if(action) return action(callback)
+
+      callback()
+    }
+
+    got(url, function(error, patch)
+    {
+      if(error) return callback(error)
+
+      applyPatches(patch,
+      {
+        loadFile: loadFile,
+        patched : patched,
+        complete: complete
+      })
+    })
+  }
+}
 
 function getName(item)
 {
@@ -31,11 +92,6 @@ function getNames(downloads)
   return result + last
 }
 
-function hasAction(item)
-{
-  return item.patch || item.action
-}
-
 
 function manager(downloads, options, callback)
 {
@@ -45,97 +101,106 @@ function manager(downloads, options, callback)
     options = {}
   }
 
-  var deps = options.deps || 'deps'
+  downloads = forceArray(downloads)
+
+  var deps = options.path || '.'
+
+  var errors = []
+  var transferred = 0
+  var length = 0
+
+  var multi = Multiprogress()
 
 
-  function addUrl(item)
+  function download(item, callback)
   {
-    this.get(item.url, join(deps, item.name))
-  }
-
-  function getAction(item)
-  {
-    var patch = item.patch
-
-    var action = item.action
-    if(action)
+    got.stream.get(item.url)
+    .on('error', callback)
+    .on('response', function(res)
     {
-      action = action.bind(item)
-      if(!patch) return action
-    }
+      const path = join(deps, item.name)
 
-    var name  = item .name
-    var path  = patch.path  || item.path  || ''
-    var strip = patch.strip || item.strip || 0
-    var url   = patch.url   || patch
+      var _error
 
-    function loadFile(patch, callback)
-    {
-      var filename = stripDirs(patch.oldFileName, strip)
-      fs.readFile(join(deps, name, path, filename), 'utf8', callback)
-    }
-
-    function patched(patch, content)
-    {
-      if(content === false)
-        return console.error('Context sanity check failed:',patch)
-
-      var filename = stripDirs(patch.newFileName, strip)
-      fs.writeFile(join(deps, name, path, filename), content)
-    }
-
-    return function(callback)
-    {
-      function complete(error)
+      // var self = this
+      function onError(error, callback)
       {
-        if(error) return callback(error)
+        if(!error) return
 
-        if(action) return action.call(item, callback)
+        _error = error
+        errors.push(error)
 
-        callback()
+        // self.abort()
+        res.once('end', function()
+        {
+          rimraf(path, callback || noop)
+        })
       }
 
-      got(url, function(error, patch)
-      {
-        if(error) return callback(error)
+//      checksums(res, item, onError)
 
-        applyPatches(patch,
-        {
-          loadFile: loadFile,
-          patched : patched,
-          complete: complete
-        })
-      })
-    }
+      // const contentLength = res.headers['content-length']
+      // if(!CI && contentLength != null)
+      // {
+      //   var bar = multi.newBar(item.name+' [:bar] :percent :etas',
+      //   {
+      //     incomplete: ' ',
+      //     width: 50,
+      //     total: parseInt(contentLength, 10)
+      //   })
+      //   res.on('data', function(chunk)
+      //   {
+      //     bar.tick(chunk.length)
+      //   })
+      // }
+
+//      pump(res, gunzip(), extract(path, {strip: 1}), function(error)
+      res.on('error', onError)
+      res.pipe(gunzip()).pipe(extract(path, {strip: 1}))
+      .on('error', onError)
+      .on('finish', callback)
+      // .on('finish', function()
+      // {
+      //   var action = getAction(item, deps)
+      //
+      //   console.trace('action 1:',item, action, callback)
+      //   if(_error || !action) return callback()
+      //   console.log('action 2:')
+      //
+      //   action(function(error)
+      //   {
+      //     console.log('action 3:',error)
+      //     if(error) return onError(error, callback)
+      //
+      //     callback()
+      //   })
+      // })
+    })
   }
 
 
-  async.reject(downloads, function(item, callback)
-  {
-    fs.exists(join(deps, item.name), callback)
-  },
-  function(downloads)
-  {
+  // async.reject(downloads, function(item, callback)
+  // {
+  //   fs.exists(join(deps, item.name), callback)
+  // },
+  // function(downloads)
+  // {
     if(!downloads.length) return callback()
 
     process.stdout.write('Downloading '+getNames(downloads)+'... ')
 
-    var download = Download({ extract: true, strip: 1 })
-
-    download.use(checksum(downloads))
-    if(!CI) download.use(progress())
-
-    downloads.forEach(addUrl, download)
-
-    download.run(function(error)
+    async.each(downloads, download, function(error)
     {
+      console.log('each:',error, errors)
       if(error) return callback(error)
+
+      if(errors.length) return callback(errors)
 
       if(CI) console.log('Done')
 
-      async.series(downloads.filter(hasAction).map(getAction),callback)
+      callback()
     })
-  })
+//  })
 }
 
 
---
+++ test/index.js
@@ -0,0 +1,93 @@
+const assert   = require('assert')
+const readFile = require('fs').readFile
+
+const dir  = require('tmp').dir.bind(null, {unsafeCleanup: true})
+const nock = require('nock')
+
+const manager = require('..')
+
+nock.disableNetConnect();
+nock.enableNetConnect('127.0.0.1');
+
+
+const A_SHA256 = 'dca3a19d32399b8654d78d83002a096df48ad6e7087f4d1bd22eeabb6d49ba32'
+const B_SHA256 = '90e533b073521133e59ef5ec77b4863fd9343cc7e5a4bd43203bc8abe88d3e01'
+
+
+const server = nock('http://example.org')
+
+const downloads =
+[
+  {
+    name: 'a_dir',
+    url: 'http://example.org/a.tar.gz',
+    sha256: A_SHA256
+  },
+  {
+    name: 'b_dir',
+    url: 'http://example.org/b.tar.gz',
+    sha256: B_SHA256
+  }
+]
+
+
+it('download and decompress one file', function(done)
+{
+  dir(function(error, path, cleanupCallback)
+  {
+    assert.ifError(error)
+
+    server.get('/a.tar.gz').replyWithFile(200, __dirname + '/fixtures/a.tar.gz',
+    {'Content-Length': 169})
+
+    manager(downloads[0], {path}, function(error)
+    {
+      assert.ifError(error)
+
+      readFile(path+'/a_dir/a1.txt', 'utf8', function(error, data)
+      {
+        assert.ifError(error)
+
+        assert.strictEqual(data, 'asdf\n')
+
+        cleanupCallback()
+        done()
+      })
+    })
+  })
+})
+
+it('download and decompress two files', function(done)
+{
+  dir(function(error, path, cleanupCallback)
+  {
+    assert.ifError(error)
+
+    server.get('/a.tar.gz').replyWithFile(200, __dirname + '/fixtures/a.tar.gz',
+    {'Content-Length': 169})
+    server.get('/b.tar.gz').replyWithFile(200, __dirname + '/fixtures/b.tar.gz',
+    {'Content-Length': 173})
+
+    manager(downloads, {path}, function(error)
+    {
+      assert.ifError(error)
+
+      readFile(path+'/a_dir/a1.txt', 'utf8', function(error, data)
+      {
+        assert.ifError(error)
+
+        assert.strictEqual(data, 'asdf\n')
+
+        readFile(path+'/b_dir/b1.txt', 'utf8', function(error, data)
+        {
+          assert.ifError(error)
+
+          assert.strictEqual(data, 'qwerty\n')
+
+          cleanupCallback()
+          done()
+        })
+      })
+    })
+  })
+})
